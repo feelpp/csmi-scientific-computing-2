4b36c677c560f3e420c953c97cd451179df5aaf6
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-12-31 16:49:16 +0100

    up

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 set -eo pipefail
 
 GITHUB_USER_NAME=$1
-mkdir -p /ssd/${USER}/feel/
-git clone https://github.com/feelpp/csmi-scientific-computing-2-${}GITHUB_USER_NAME}.git
-cd csmi-scientific-computing-2-${}GITHUB_USER_NAME}
-cp -r /data/csmi/calcul-scientifique/HL-31_H1 .
+mkdir -p /ssd/${USER}/feel/$0
+git clone https://github.com/feelpp/csmi-scientific-computing-2-${GITHUB_USER_NAME}.git
+cd csmi-scientific-computing-2-${GITHUB_USER_NAME}
+rsync -avz /data/csmi/calcul-scientifique/HL-31_H1 .
 
 
 echo "You are now setup to do the homework!"
