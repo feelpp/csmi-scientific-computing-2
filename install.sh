3ba9a5e08b7831b39cce55d6ae2626e253a48fdd
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-01-02 05:19:08 +0100

    fix dir

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 GITHUB_USER_NAME=$1
 mkdir -p /ssd/${USER}/feel/
+cd /ssd/${USER}/feel/
 git clone https://github.com/feelpp/csmi-scientific-computing-2-${GITHUB_USER_NAME}.git $0
 cd $0 && rsync -avz /data/csmi/calcul-scientifique/HL-31_H1 .
 
